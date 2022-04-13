83b089568bbdcbde001cc2017c7126937da34d34
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-13 00:08:13 -0600

    detalles

--- views/dashboard/principal.php
+++ views/dashboard/principal.php
@@ -47,10 +47,8 @@ Dashboard_Page::headerTemplate('Home');
 						</div>
 					</div>
 					<nav>
-						<a id="bb-nav-first" href="#" class="bb-custom-icon bb-custom-icon-first">First page</a>
 						<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
 						<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
-						<a id="bb-nav-last" href="#" class="bb-custom-icon bb-custom-icon-last">Last page</a>
 					</nav>
 				</div>
 			</div>
