a07489f5693593d892c081ae9594da615df3eeb8
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-14 02:12:19 -0600

    avance minimo disenio index para avanzar un poco

--- views/dashboard/principal.php
+++ views/dashboard/principal.php
@@ -4,61 +4,63 @@ require_once('../../app/helpers/dashboard_page.php');
 //Se imprime la plantilla del encabezado y se envía el titulo para la página web
 Dashboard_Page::headerTemplate('Home');
 ?>
-		<link rel="stylesheet" type="text/css" href="../../resources/css/demo1.css" />
-        <link rel="stylesheet" type="text/css" href="../../resources/css/bookblock.css" />
-		<script src="../../resources/js/modernizr.custom.js"></script>
-        <script src="../../resources/js/principal.js"></script>
+<link rel="stylesheet" type="text/css" href="../../resources/css/demo1.css" />
+<link rel="stylesheet" type="text/css" href="../../resources/css/bookblock.css" />
+<link rel="stylesheet" type="text/css" href="../../resources/css/_sections.scss" />
+<script src="../../resources/js/modernizr.custom.js"></script>
+		<section id="services" class="section">
+			<div class="contenedor">
+					<main>
+						<div class="contenedor-img">
+								<img src="../../resources/img/logo_sinai.png" alt="">
+						</div>
+						<div class="contenedor-texto">
+								<h2 class="titulo">¿Quieres aprender <br>  <span class="typed"></span> ? </h2>
+								<p>MonteSinai es una gran ventana a la formacion en todos los sentidos, nuestros libros le ayudan en la aventura del saber, del conocer, de convivir y descubrir. Es un recurso didáctico elaborado con la intención de facilitar los procesos de enseñanza y aprendizaje. Contamos con un portafolio de libros de texto curriculares y no curriculares</p>
 
-
-<div class="contenedor">
-          <main>
-               <div class="contenedor-img">
-                    <img src="../../resources/img/logo_sinai.png" alt="">
-               </div>
-               <div class="contenedor-texto">
-					<h2 class="titulo">¿Quieres aprender <br>  <span class="typed"></span> ? </h2>
-                    <p>MonteSinai es una gran ventana a la formacion en todos los sentidos, nuestros libros le ayudan en la aventura del saber, del conocer, de convivir y descubrir. Es un recurso didáctico elaborado con la intención de facilitar los procesos de enseñanza y aprendizaje. Contamos con un portafolio de libros de texto curriculares y no curriculares</p>
-
-                    <a href="../dashboard/login.php" class="btn-link activo">Nuestros Libros</a>
-                    <a href="../dashboard/sobre_nosotros.php" class="btn-link">Conocenos</a>
-               </div>
-          </main>
-</div>
-<div class="css-xfq28i"></div>
-<br>
-<div class="container">
-			<div class="main clearfix">
-				<div class="bb-custom-wrapper">
-					<div id="bb-bookblock" class="bb-bookblock">
-						<div class="bb-item">
-							<a href=""><img src="../../resources/img/1.jpg" alt="image01"/></a>
+								<a href="../dashboard/login.php" class="btn-link activo">Nuestros Libros</a>
+								<a href="../dashboard/sobre_nosotros.php" class="btn-link">Conocenos</a>
 						</div>
-						<div class="bb-item">
-							<a href=""><img src="../../resources/img/2.jpg" alt="image02"/></a>
+					</main>
+			</div>
+		</section>
+		<section class="services-list parallax2">
+			<div class="container">
+    				<div class="typing-demo">
+      					Ejemplos:
+    				</div>
+				<div class="main clearfix">
+					<div class="bb-custom-wrapper">
+						<div id="bb-bookblock" class="bb-bookblock">
+							<div class="bb-item">
+								<a href=""><img src="../../resources/img/1.jpg" alt="image01"/></a>
+							</div>
+							<div class="bb-item">
+								<a href=""><img src="../../resources/img/2.jpg" alt="image02"/></a>
+							</div>
+							<div class="bb-item">
+								<a href=""><img src="../../resources/img/3.jpg" alt="image03"/></a>
+							</div>
+							<div class="bb-item">
+								<a href=""><img src="../../resources/img/4.jpg" alt="image04"/></a>
+							</div>
+							<div class="bb-item">
+								<a href=""><img src="../../resources/img/5.jpg" alt="image05"/></a>
+							</div>
 						</div>
-						<div class="bb-item">
-							<a href=""><img src="../../resources/img/3.jpg" alt="image03"/></a>
-						</div>
-						<div class="bb-item">
-							<a href=""><img src="../../resources/img/4.jpg" alt="image04"/></a>
-						</div>
-						<div class="bb-item">
-							<a href=""><img src="../../resources/img/5.jpg" alt="image05"/></a>
-						</div>
+						<nav>
+							<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
+							<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
+						</nav>
 					</div>
-					<nav>
-						<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
-						<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
-					</nav>
 				</div>
 			</div>
-		</div><!-- /container -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="../../resources/js/jquerypp.custom.js"></script>
-		<script src="../../resources/js/jquery.bookblock.js"></script>
-		<script>
-			var Page = (function() {
-				
+	</section>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="../../resources/js/jquerypp.custom.js"></script>
+	<script src="../../resources/js/jquery.bookblock.js"></script>
+	<script>
+		var Page = (function() {
 				var config = {
 						$bookBlock : $( '#bb-bookblock' ),
 						$navNext : $( '#bb-nav-next' ),
@@ -75,10 +77,7 @@ Dashboard_Page::headerTemplate('Home');
 						initEvents();
 					},
 					initEvents = function() {
-						
 						var $slides = config.$bookBlock.children();
-
-						// add navigation events
 						config.$navNext.on( 'click touchstart', function() {
 							config.$bookBlock.bookblock( 'next' );
 							return false;
@@ -109,7 +108,6 @@ Dashboard_Page::headerTemplate('Home');
 								return false;
 							}
 						} );
-
 						// add keyboard events
 						$( document ).keydown( function(e) {
 							var keyCode = e.keyCode || e.which,
@@ -130,31 +128,83 @@ Dashboard_Page::headerTemplate('Home');
 							}
 						} );
 					};
-
 					return { init : init };
-
 			})();
 		</script>
 		<script>
 				Page.init();
 		</script>
-        <div class="css-xfq28i"></div>
-        <br>
-      
-
-    <!-- quick cache hack -->
-    <img src="../../resources/img/1.jpg" style="display: none">
-    <img src="../../resources/img/2.jpg" style="display: none">
-    <img src="../../resources/img/3.jpg" style="display: none">
-    <img src="../../resources/img/4.jpg" style="display: none">
-    <img src="../../resources/img/5.jpg" style="display: none">
-
-
-
-	 <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
-     <script src="../../resources/js/main.js"></script>
-     <link rel="stylesheet" href="../../resources/css/style.css"></script>
-
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="blog-card spring-fever">
+					<div class="title-content">
+						<h3><a href="#">Los mejores en educacion</a></h3>
+						<div class="intro"> <a href="#">Educacion</a> </div>
+						</div>
+						<div class="card-info">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
+						</div>
+						<div class="gradient-overlay"></div>
+							<div class="color-overlay"></div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="blog-card2 spring-fever">
+					<div class="title-content">
+						<h3><a href="#">10 inspiring photos</a></h3>
+						<div class="intro"> <a href="#">Inspiration</a> </div>
+						</div>
+						<div class="card-info">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
+						</div>
+						<div class="gradient-overlay"></div>
+							<div class="color-overlay"></div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="blog-card spring-fever">
+					<div class="title-content">
+						<h3><a href="#">Los mejores en educacion</a></h3>
+						<div class="intro"> <a href="#">Educacion</a> </div>
+						</div>
+						<div class="card-info">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
+						</div>
+						<div class="gradient-overlay"></div>
+							<div class="color-overlay"></div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="blog-card2 spring-fever">
+					<div class="title-content">
+						<h3><a href="#">10 inspiring photos</a></h3>
+						<div class="intro"> <a href="#">Inspiration</a> </div>
+						</div>
+						<div class="card-info">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
+						</div>
+						<div class="gradient-overlay"></div>
+							<div class="color-overlay"></div>
+				</div>
+			</div>
+		</div>
+        <br><br><br><br><br><br>
+		<section class="static-testimonial parallax5">
+			<div class="container">
+				<blockquote>
+					<i class="fa fa-quote-left"></i>
+					<i class="fa fa-quote-right"></i>"Intenta aprender algo sobre todo y todo sobre algo"
+					<span>Thomas Huxley</span>
+				</blockquote>
+			</div>
+		</section>
+		<div class="css-xfq28i"></div>
+		<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+		<script src="../../resources/js/main.js"></script>
+		<link rel="stylesheet" href="../../resources/css/style.css"></script>
 <?php
 // Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
 Dashboard_Page::footerTemplate('main.js');
