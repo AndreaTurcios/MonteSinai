7494b376f90258cc75f7aa909ac411b4319aee06
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-18 05:37:45 -0600

    name

--- views/dashboard/main.php
+++ views/dashboard/main.php
@@ -2,7 +2,7 @@
 //Se incluye la clase con las plantillas del documento
 require_once("../../app/helpers/dashboard_page.php");
 //Se imprime la plantilla del encabezado y se envía el titulo para la página web
-Dashboard_Page::headerTemplate('Empleados');
+Dashboard_Page::headerTemplate('Libros');
 ?>
 <!--Aquí comenzamos abriendo la sección -->
 <section>
