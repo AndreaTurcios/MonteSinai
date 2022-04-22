b4461975744b7832966f460491ab62738802d98b
Shephiros <erickcampos.rivas@gmail.com>
2022-04-21 23:11:46 -0600

    Revisar maquetacion

--- views/dashboard/Libro Cuatro.php
+++ views/dashboard/Libro Cuatro.php
@@ -23,15 +23,38 @@ Dashboard_Page::headerTemplate('Libro 1');
         </div>
 
         <div class="row">
-            <div class="col-4" style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
-                <div>
-                    <div style="border-bottom-color: black; border-bottom-style: solid; border-bottom-width: 1px; ">1</div>
-                    <div style="margin-bottom: 20px;">My body</div>
-                    <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B;">Primary</button></div>
+            <!--Unidad 1 -->
+            <div class="col-sm-4" style=" padding: 10px;">
+                <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
+                    <div style="border-bottom-color: black;  ">
+                        <span style="font-weight: bold;">1</span>
+                    </div>
+                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My body</div>
+                    <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
                 </div>
             </div>
-            <div class="col-4"></div>
-            <div class="col-4"></div>
+            
+            <!--Unidad 2 -->
+            <div class="col-sm-4" style=" padding: 10px;">
+                <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
+                    <div style="border-bottom-color: black;  ">
+                        <span style="font-weight: bold;">2</span>
+                    </div>
+                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My Family</div>
+                    <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
+                </div>
+            </div>
+            
+            <!--Unidad 3 -->
+            <div class="col-sm-4" style=" padding: 10px;">
+                <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
+                    <div style="border-bottom-color: black;  ">
+                        <span style="font-weight: bold;">3</span>
+                    </div>
+                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My Classroom</div>
+                    <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
