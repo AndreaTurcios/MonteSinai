bbf2bb6fbb58db27597fe5b33705798cc510cbd2
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-22 11:52:58 -0600

    units

--- views/dashboard/FIRST GRADE.php
+++ views/dashboard/FIRST GRADE.php
@@ -13,13 +13,14 @@ Dashboard_Page::headerTemplate('Libro 1');
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 text-center" id="Titulo1">
-                <h1 class="center">BOOK<br />TIME TO LEARN ENGLISH</h1>
+                <h1 class="center">FIRST BOOK: <br />Time to learn english</h1>
                 <br />
-                <h3 class="center">FIRST GRADE</h3>
+                <h3 class="center">First grade</h3>
             </div>
         </div>
-        <div class="row" style="padding: 10px; background-color: #26272b; margin-top: 10px; margin-bottom: 10px; border-radius: 10px;">
-            <div class="col-12" style="color: white; font-weight: bold;">UNIT</div>
+        <div class="row" style="padding: 10px; background-color: #26272b; margin-top: 10px; 
+            margin-bottom: 10px; border-radius: 10px;">
+            <div class="col-12" style="color: white; font-weight: bold;">UNITS</div>
         </div>
 
         <div class="row">
@@ -27,31 +28,52 @@ Dashboard_Page::headerTemplate('Libro 1');
             <div class="col-sm-4" style=" padding: 10px;">
                 <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
                     <div style="border-bottom-color: black;  ">
-                        <span style="font-weight: bold;">1</span>
+                        <div style="margin-top: 10px; font-weight: bold; 
+                        text-transform: uppercase; font-size: 18px;">UNIT 1</div>
                     </div>
-                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My body</div>
+
+                    <span style="font-weight: bold;text-transform: uppercase; font-size: 17px;">My Body</span>
+                    <div style="
+                        font-size: 18px;">- My body
+                    </div>
+                    <div style="margin-bottom: 25px; 
+                        font-size: 18px;">- Food and fruits
+                    </div>
                     <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
                 </div>
             </div>
-            
+
             <!--Unidad 2 -->
             <div class="col-sm-4" style=" padding: 10px;">
                 <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
                     <div style="border-bottom-color: black;  ">
-                        <span style="font-weight: bold;">2</span>
+                    <div style="margin-top: 10px; font-weight: bold; 
+                        text-transform: uppercase; font-size: 18px;">UNIT 2</div>
                     </div>
-                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My Family</div>
+                    <span style="font-weight: bold;text-transform: uppercase; font-size: 17px;">My Family</span>
+                    <div style="
+                        font-size: 18px;">- My family
+                    </div>
+                    <div style="margin-bottom: 25px; 
+                        font-size: 18px;">- Busy People
+                    </div>
                     <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
                 </div>
             </div>
-            
+
             <!--Unidad 3 -->
             <div class="col-sm-4" style=" padding: 10px;">
                 <div style="background-color: #E4E4E4; border-radius: 10px; padding: 10px;">
                     <div style="border-bottom-color: black;  ">
-                        <span style="font-weight: bold;">3</span>
+                        <div style="margin-top: 10px; font-weight: bold; 
+                        text-transform: uppercase; font-size: 18px;">UNIT 3</div>
                     </div>
-                    <div style="margin-top: 10px;margin-bottom: 25px; font-weight: bold; text-transform: uppercase; font-size: 18px;">My Classroom</div>
+                    <span style="font-weight: bold;text-transform: uppercase; font-size: 17px;">My School</span>
+                    <div style="font-size: 18px;">- My Classroom
+                    </div>
+                    <div style="margin-bottom: 25px; 
+                        font-size: 18px;">- My Toys
+                    </div>
                     <div><button type="button" class="btn btn-primary" style="width: 100%; background-color: #2A262B; border-color: #2A262B; ">Enter</button></div>
                 </div>
             </div>
