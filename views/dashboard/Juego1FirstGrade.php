34ef24459188f520a3265f63a399f9e492826539
arwenKonne <78279066+arwenKonne@users.noreply.github.com>
2022-04-25 22:51:04 -0600

    Estructura Juego 1

---
+++ views/dashboard/Juego1FirstGrade.php
@@ -0,0 +1,274 @@
+<?php
+//Se incluye la clase con las plantillas del documento
+require_once("../../app/helpers/dashboard_page.php");
+//Se imprime la plantilla del encabezado y se envía el titulo para la página web
+Dashboard_Page::headerTemplate('Actividad 1');
+?>
+
+<div class="card border border-dark">
+    <div class="container">
+        <div class="form-group">
+            <!-- columna -->
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-8 align-items-center">
+                        <p class="fs-1 fw-bold">Complete the activity</p>
+                    </div>
+                </div>
+                <div class="row row-cols-4">
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">H</div>
+                                <div class="col">e</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">d</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">a</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">r</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">e</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">r</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-6">
+                                <div class="col">m</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">o</div>
+                                <div class="col">u</div>
+                                <div class="col">t</div>
+                                <div class="col">h</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <!-- espacio -->
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <!-- espacio -->
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">H</div>
+                                <div class="col">e</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">k</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">n</div>
+                                <div class="col"><input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1"></div>
+                                <div class="col">s</div>
+                                <div class="col">e</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">h</div>
+                                <div class="col">a</div>
+                                <div class="col">n</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">e</div>
+                                <div class="col">g</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <!-- espacio -->
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <!-- espacio -->
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">F</div>
+                                <div class="col">o</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">t</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">a</div>
+                                <div class="col">i</div>
+                                <div class="col">r</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-7">
+                                <div class="col">s</div>
+                                <div class="col">t</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">m</div>
+                                <div class="col">a</div>
+                                <div class="col">c</div>
+                                <div class="col">h</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-6">
+                                <div class="col">f</div>
+                                <div class="col">i</div>
+                                <div class="col">n</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">e</div>
+                                <div class="col">r</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <!-- espacio -->
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <!-- espacio -->
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col"> <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1"> </div>
+                                <div class="col">a</div>
+                                <div class="col">c</div>
+                                <div class="col">e</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">o</div>
+                                <div class="col">e</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">e</div>
+                                <div class="col">
+                                    <input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">e</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                    <div class="col border border-dark">
+                        <!-- inicio group -->
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="row row-cols-4">
+                                <div class="col">k</div>
+                                <div class="col">n</div>
+                                     <div class="col"><input type="text" class="col-6 col-md-4 form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" maxlength="1">
+                                </div>
+                                <div class="col">e</div>
+                            </div>
+                        </div>
+                        <!-- fin group -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Botones de Control -->
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar">Submit</button><br>
+    </div>
+</div>
+
+
+
+<?php
+// Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
+Dashboard_Page::footerTemplate('libros.js');
+?>
