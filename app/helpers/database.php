dbd3ae7f7b5338825100ca80c4fd55319bffd872
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-27 08:08:05 -0600

    a

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -20,7 +20,7 @@ class Database
         //$username = 'root';
         $username = 'postgres';
         //$password = '';
-        $password = 'jkl555';
+        $password = 'admin';
    
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:  host='.$server.';dbname='.$database.';port=5432', $username, $password);
