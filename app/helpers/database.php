9ab6fa50406ce253f70ea40f0213f3ce66dfa1e9
AndreaTurcios <turciosbarquero@gmail.com>
2022-05-02 16:40:32 -0600

    cambio db

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -27,7 +27,7 @@ class Database
 
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:  host='.$server.';dbname='.$database.
-        ';port=5432', $username, $passwordAdal);
+        ';port=5432', $username, $passwordAndrea);
     }
 
     /*
