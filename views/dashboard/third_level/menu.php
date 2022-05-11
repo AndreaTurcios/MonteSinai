d867a49a7611da5996de2a828f9493dc273a3b5e
Dony Navas <69410684+donynavas@users.noreply.github.com>
2022-05-10 19:32:32 -0700

    Add files via upload

---
+++ views/dashboard/third_level/menu.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<!-- saved from url=(0060)https://getbootstrap.com/docs/4.0/examples/starter-template/ -->
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Third Level</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#"><img src="img/logosinai.png" width="250px" height=""> </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"><span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link disabled" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"></a>
+          </li>
+         
+        </ul>
+       
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+<table class="table table-borderless">
+  <tr>
+    <td></td>
+      <td>
+      <div class="starter-template">
+        <div class="card" style="width: 18rem;">
+        <div align="center"><img src="img/mybody.png" alt="..." width="88%" height="384" class="card-img-top">        </div>
+        <div class="card-body">
+        <h5 class="card-title">UNIT ONE</h5>
+        <p class="card-text">-I LIKE TO EAT.<br>-A BUSY FAMILY</p>
+      <a href="lesson-one-bodyparts.php" class="btn btn-primary">LET'S GO</a>
+  </div>
+    <td>
+        <div class="starter-template">
+        <div class="card" style="width: 18rem;">
+        <div align="center"><img src="img/unit2.png" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">UNIT TWO</h5>
+        <p class="card-text">-A HAPPY PARTY<br>-MEANS OF TRANSPORTATION</p>
+      <a href="lesson-two.php" class="btn btn-primary">LET'S GO</a>
+  </div>
+   <td>
+      <div class="starter-template">
+        <div class="card" style="width: 18rem;">
+        <div align="center"><img src="img/unit3.png" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">UNIT THREE</h5>
+        <p class="card-text">-TELLING THE TIME<br>-DAILY ACTIVITIES<br>.</p>
+      <a href="lesson-three.php" class="btn btn-primary">LET'S GO</a>
+  </div>
+</td>
+</tr>
+</table>
+     
+ 
+</div>
+        <h3></h3>
+        <p class="lead"></p>
+      </div>
+     
+
+
+
+
+
+
+
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="css/jquery-3.2.1.slim.min.js.descarga" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="./Starter Template for Bootstrap_files/popper.min.js.descarga"></script>
+    <script src="./Starter Template for Bootstrap_files/bootstrap.min.js.descarga"></script>
+  
+
+</body></html>
