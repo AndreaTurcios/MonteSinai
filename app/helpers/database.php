12c89d4ff5c1ac20f647f33b7451d3b709a33e03
Erick Campos <erickcampos.rivas@gmail.com>
2022-05-15 00:48:49 -0600

    Libro 5
    Libro 5 base

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -22,8 +22,8 @@ class Database
         //Cambiar dependiendo de la persona, pero si pueden de un solo denle git ignore 
         //para no estar cambiando a cada rato
         $passwordAndreaAndMaza = 'admin';
-        $passwordErick = '';
-        $password = 'jkl555';
+        $password = 'admin';
+        $password1 = 'jkl555';
 
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:  host='.$server.';dbname='.$database.
