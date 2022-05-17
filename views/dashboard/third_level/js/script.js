11729c557e7e0c1208c60662e755b0577ffad60e
Dony Navas <donyhenry@hotmail.com>
2022-05-17 06:44:38 -0700

    Cambios

--- views/dashboard/third_level/js/script.js
+++ views/dashboard/third_level/js/script.js
@@ -1,23 +1,61 @@
-function onDragStart(event) {
-  event
-    .dataTransfer
-    .setData('text/plain', event.target.id);
+var elementoArrastrado;
 
-    event
-    .currentTarget
-    .style
-    .backgroundColor = 'yellow';
-}
+ 
+  document.addEventListener("drag", function( event ) {
+  //console.log("drag")
+  }, false);
 
-function onDragOver(event) {
-  event.preventDefault();
-}
 
-function onDrop(event) {
-  const id = event
-    .dataTransfer
-    .getData('text');
-    const draggableElement = document.getElementById(id);
-    const bodyparts = event.target;
-    bodyparts.appendChild(draggableElement);
-}
+  document.addEventListener("dragstart", function( event ) {
+    // guarda información acerca del objeto arrastrado
+    event.dataTransfer.setData('text/plain',null)
+      // guarda una referéncia del elemento arrastrado
+      elementoArrastrado = event.target;
+      // cambia la opacidad del elemento a medio transparente
+      event.target.style.opacity = .5;
+  }, false);
+
+
+  document.addEventListener("dragend", function( event ) {
+      // reestablece el valor de la opacidad
+      event.target.style.opacity = 1;
+  }, false);
+
+ 
+  document.addEventListener("dragover", function( event ) {
+      // previene el comportamiento por defecto del elemento arrastrado
+     event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function( event ) {
+      // comprueba si el event.target es una zona de soltar  
+      if ( event.target.className == "zona-de-soltar" ) {
+        // y di lo és cambia el color de fondo
+          event.target.style.background = "purple";
+      }
+
+  }, false);
+
+  document.addEventListener("dragleave", function( event ) {
+      // comprueba si el event.target es una zona de soltar  
+      if ( event.target.className == "zona-de-soltar"  ) {
+        // y si lo és, reestablece el valor inicial
+          event.target.style.background = "";
+      }
+  }, false);
+
+  document.addEventListener("drop", function( event ) {
+      // Si el elemento arrastrado es un link, este se abre en una nueve página.
+      // Para que esto no pase hay que utilizar: 
+      event.preventDefault();
+      // comprueba si el event.target es una zona de soltar
+      if ( event.target.className == "zona-de-soltar"  ) {
+          // reestablece el valor inicial para el background
+          event.target.style.background = "";
+          // elimina el elemento arrastrado del del elemento padre
+          elementoArrastrado.parentNode.removeChild( elementoArrastrado );
+          // y lo agrega al elemento de destino
+          event.target.appendChild( elementoArrastrado );
+      }
+    
+  }, false);
--- views/dashboard/third_level/lesson-bodyparts.php
+++ views/dashboard/third_level/lesson-bodyparts.php
@@ -21,227 +21,191 @@
     <link href="css/starter-template.css" rel="stylesheet">
 
      <link rel="stylesheet" href="css/style.css" />
-     <style type="text/css">
-
-     </style>
 </head>
-
   <body>
 
-  
-
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#"><img src="img/logosinai.png" width="250px" height=""> </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"><span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"></a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link disabled" href="https://getbootstrap.com/docs/4.0/examples/starter-template/#"></a>
-          </li>
-         
-        </ul>
-       
+   <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <div align="center"><a class="navbar-brand" href="#"><img src="img/logosinai.png" width="250px" height=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
       </div>
-    </nav>
-<br>
-<br>
-  <main role="main" class="container">
-      <h3 align="center">BODY PARTS . </h3>
-      <table width="926" border="1" align="center" bordercolor="#FFFFFF" 
-     background="img/bodyparts.png" style="background-repeat:no-repeat; background-position: 30% 60%; 
-     background-attachment: fixed;">
-      <tr>
-        <th colspan="6" rowspan="14" bordercolor="#000000" scope="col">&nbsp;</th>
-        <th bordercolor="#000000" scope="col">Image</th>
-        <th bordercolor="#000000" scope="col">Match</th>
-        <th bordercolor="#000000" scope="col">Image</th>
-        <th bordercolor="#000000" scope="col">Match</th>
-      </tr>
-      <tr>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000" scope="col">HAIR </td>
-        <td bordercolor="#000000" scope="col">
-        <div
-          class="bodyparts"
-          ondragover="onDragOver(event);"
-          ondrop="onDrop(event);"
-        >        </div>        </td>
-        <td bordercolor="#000000" scope="col">EYE</td>
-        <td bordercolor="#000000" scope="col">OK</td>
-      </tr>
-      <tr>
-        <td width="60" bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col"><div></td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-      </tr>
-      <tr>
-        <td width="60" bordercolor="#000000" scope="col">HEAD</td>
-        <td bordercolor="#000000" scope="col">OK</td>
-        <td bordercolor="#000000" scope="col">EAR</td>
-        <td width="129" bordercolor="#000000" scope="col">OK</td>
-      </tr>
-      <tr>
-        <td width="60" bordercolor="#000000" scope="col">&nbsp;</td>
-        <td width="92" bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-      <tr>
-        <td width="60" bordercolor="#000000" scope="col">NOSE</td>
-        <td bordercolor="#000000">OK</td>
-        <td bordercolor="#000000" scope="col">ARM</td>
-        <td bordercolor="#000000">OK</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td width="91" bordercolor="#000000" scope="col">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">HAND</td>
-        <td bordercolor="#000000">OK</td>
-        <td width="91" bordercolor="#000000" scope="col">LEG</td>
-        <td bordercolor="#000000">OK</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">FOOT</td>
-        <td bordercolor="#000000">OK</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-        
-      <tr>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-      <tr>
-        <td width="71" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        HAIR</div></div></td>
-        <td width="77" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        HEAD</div></div></td>
-        <td width="65" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        EYE</div></div></td>
-        <td width="90" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        MOUTH</div></div></td>
-        <td width="76" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        NOSE</div></div></td>
-        <td width="111" bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        LEG</div></div></td>
-        <td bordercolor="#000000"><div align="center">
-          <div
-          id="draggable-1"
-          class="example-draggable"
-          draggable="true"
-          ondragstart="onDragStart(event);"
-        >
-        FOOT</div></div></td>
-        <td bordercolor="#000000"><div align="center"></div></td>
-        <td bordercolor="#000000">&nbsp;</td>
-        <td bordercolor="#000000">&nbsp;</td>
-      </tr>
-    </table>
-    <p>&nbsp;</p>
-    <p><br>
-      <script src="js/script.js"></script>
-    </p>
-      
-     
+   </nav>
+   <div align="left"><br>
+   <br>
   </div>
-  <div
-    class="bodyparts"
-    ondragover="onDragOver(event);"
-    ondrop="onDrop(event);"
-  >
-    Parts of the body  </div>
-</div>
-  </main>
-    <!-- /.container -->
-
-    <!-- Bootstrap core JavaScript
+  <main role="main" class="container">  </main>
+    <div align="center">
+      <!-- /.container -->
+      
+      <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="css/jquery-3.2.1.slim.min.js.descarga" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="./Starter Template for Bootstrap_files/popper.min.js.descarga"></script>
-    <script src="./Starter Template for Bootstrap_files/bootstrap.min.js.descarga"></script>
-  
-
-
-<map name="Map">
-  <area shape="rect" coords="119,30,143,40" href="#" target="_self" alt="Hair">
-</map>
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="css/jquery-3.2.1.slim.min.js.descarga" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    </div>
+    <table width="843" height="340"  border="1" align="center" cellspacing="5" bordercolor="#FFFFFF" background="img/bodyparts.png" style="background-repeat: no-repeat; background-position: center center;">
+      <tr>
+        <th width="53" scope="col">&nbsp;</th>
+        <th width="53" scope="col">&nbsp;</th>
+        <th width="20" scope="col">&nbsp;</th>
+        <th width="54" scope="col">&nbsp;</th>
+        <th width="103" scope="col">&nbsp;</th>
+        <th width="199" rowspan="10" scope="col">&nbsp;</th>
+        <td width="100" bgcolor="#FFCC66">HEAD</td>
+        <th width="79" scope="col">&nbsp;</th>
+        <th width="53" scope="col">&nbsp;</th>
+        <th width="65" scope="col">&nbsp;</th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">EYE</td>
+        <td bgcolor="#FFCC66">HAIR</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">EYELASH</td>
+        <td bgcolor="#FFCC66">EAR</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">SHOULDER</td>
+        <td bgcolor="#FFCC66">MOUTH</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">ARM</td>
+        <td bgcolor="#FFCC66">HAND</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">KNEE</td>
+        <td bgcolor="#FFCC66">LEG</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td bgcolor="#FFCC66">FOOT</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+  </table>
+    
+        <p>&nbsp;</p>
+        <form name="form1" method="post" action="">
+          <h3 align="center">My Body </h3>
+          <table width="1040" border="1" align="center">
+            <tr>
+              <th width="76" scope="col"><div align="center">HEAD</div></th>
+              <th width="66" scope="col"><div align="center">HAIR</div></th>
+              <th width="83" scope="col"><div align="center">EYE</div></th>
+              <th width="81" scope="col"><div align="center">EYELASH</div></th>
+              <th width="141" scope="col"><div align="center">SHOULDER</div></th>
+              <th width="74" scope="col"><div align="center">NOSE</div></th>
+              <th width="102" scope="col"><div align="center">MOUTH</div></th>
+              <th width="62" scope="col"><div align="center">ARM</div></th>
+              <th width="82" scope="col"><div align="center">HAND</div></th>
+              <th width="71" scope="col"><div align="center">KNEE</div></th>
+              <th width="49" scope="col"><div align="center">LEG</div></th>
+              <th width="77" scope="col"><div align="center">FOOT</div></th>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          </table>
+  </form>
+        <p>&nbsp;</p>
+        <p>
+          <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+          <script src="./Starter Template for Bootstrap_files/popper.min.js.descarga"></script>
+          <script src="./Starter Template for Bootstrap_files/bootstrap.min.js.descarga"></script>
+          
+          
+          
+          
+          
+            </div>
+        </p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;      </p>
   </body></html>
 
 
