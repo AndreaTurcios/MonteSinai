1610fa3759ab4e2f53bd619eb559be028d9953d5
oemancia <oemancia@hotmail.com>
2022-05-26 13:29:53 -0600

    update juego libro 2

--- views/dashboard/libro2Unidad1t.php
+++ views/dashboard/libro2Unidad1t.php
@@ -497,19 +497,62 @@ Book_Page::headerTemplate('UNIT ONE "MY BODY"');
 	</div>
 </div>
 
-<div class="modal fade" id="ModalLibrotest" tabindex="-2" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<div class="modal-body">
-
-			</div>
-		</div>
-	</div>
+<div id="ModalLibroDos" class="modal fade" tabindex="-11">
+    <!-- <div class="container-fluid"> -->
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Complete the sentences:</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" id="game-one">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                    
+                        <div class="row">
+                            <div class="col-6">
+								<div class="pregunta">
+									<h5>Her </h5> <input id="q1"type="text" style="width:75px"> <h5>is red.</h5> 
+								</div>
+								<div class="pregunta">
+									<h5>Question 2</h5>
+									<input id="q2" type="text" style="width:450px">
+								</div>
+								<div class="pregunta">
+									<h5>Question 3</h5>
+									<input id="q3" type="text" style="width:450px">
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="respuesta">
+									<h5>Answer 1</h5>
+									<input id="a1" type="text">
+								</div>
+								<div class="respuesta">
+									<h5>Answer 2</h5>
+									<input id="a2" type="text">
+								</div>
+								<div class="respuesta">
+									<h5>Answer 3</h5>
+									<input id="a3" type="text">
+								</div>
+							</div>
+                        </div>
+                    </div>
+                    <br>
+                    <!-- Botones de Control -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn waves-effect blue tooltipped"
+                            data-tooltip="Guardar">Submit</button>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
 
 
 <!-- --------------------------------- inicio plantilla footer  ---------------------------------	 -->
