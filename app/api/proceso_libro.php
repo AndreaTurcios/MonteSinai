8c51e894b06afef6eabce3a3c81bb1be50be9d35
Maza17 <dinnx247@gmail.com>
2022-05-27 09:25:13 -0600

    Actividad 2 Unidad 1 Libro 11

--- app/api/proceso_libro.php
+++ app/api/proceso_libro.php
@@ -456,6 +456,27 @@ if (isset($_GET['action'])) {
                         $result['exception'] = 'puntos incorrecto';
                     }
                 break;
+                case 'createactA2U1L11':
+                    $_POST = $libros->validateForm($_POST);
+                    if ($libros->setPromedio($_POST['pointsA2U1L11'])) {
+                        if ($libros->setLibro($_POST['idlibroA2U1L11'])) {
+                            if ($libros->setId($_POST['idclienteA2U1L11'])) {
+                                if ($libros->createRow()) {
+                                    $result['status'] = 1;
+                                    $result['message'] = 'success';
+                                } else {
+                                    $result['exception'] = Database::getException();;
+                                }
+                            } else {
+                                $result['exception'] = 'cliente incorrecto';
+                            }
+                        } else {
+                            $result['exception'] = 'libro incorrecto';
+                        }
+                    } else {
+                        $result['exception'] = 'puntos incorrecto';
+                    }
+                break;
             default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
--- views/dashboard/libro11unidad1.php
+++ views/dashboard/libro11unidad1.php
@@ -488,6 +488,78 @@ yepnope({
     </div>
 </div>
 
+<div id="ModalLibroDos" class="modal fade" tabindex="-4">
+    <!-- <div class="container-fluid"> -->
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Complete the activity - Write question and answers expressing thoughts about famous people.</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" id="game-two">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+							<div class="col-md-8 align-items-center">
+								<!-- class="d-none" -->
+								<input type="text"  class="d-none" id="pointsA2U1L11" name="pointsL11">
+                                <input type="text"  class="d-none" id="idlibroA2U1L11" name="idlibroL11">
+								<input type="text"  class="d-none" id="idclienteA2U1L11" name="idclienteL11">
+							</div>
+						</div>
+                        <div class="row">
+                            <div class="col-6">
+								<div class="pregunta">
+									<h5>Question 1</h5>
+									<input id="q1"type="text" style = "width:60%">
+								</div>
+								<div class="pregunta">
+									<h5>Question 2</h5>
+									<input id="q2" type="text" style = "width:60%">
+								</div>
+								<div class="pregunta">
+									<h5>Question 3</h5>
+									<input id="q3" type="text" style = "width:60%">
+								</div>
+                                <div class="pregunta">
+									<h5>Question 4</h5>
+									<input id="q4" type="text" style = "width:60%">
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="respuesta">
+									<h5>Answer 1</h5>
+									<input id="a1" type="text" style = "width:60%">
+								</div>
+								<div class="respuesta">
+									<h5>Answer 2</h5>
+									<input id="a2" type="text" style = "width:60%">
+								</div>
+								<div class="respuesta">
+									<h5>Answer 3</h5>
+									<input id="a3" type="text" style = "width:60%">
+								</div>
+                                <div class="respuesta">
+									<h5>Answer 4</h5>
+									<input id="a4" type="text" style = "width:60%">
+								</div>
+							</div>
+                        </div>
+                    </div>
+                    <br>
+                    <!-- Botones de Control -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn waves-effect blue tooltipped"
+                            data-tooltip="Guardar">Submit</button>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 
 <!-- --------------------------------- inicio plantilla footer  ---------------------------------	 -->
 <?php
