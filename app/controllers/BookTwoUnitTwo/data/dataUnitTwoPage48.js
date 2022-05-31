b0da641d5f5423caf7fb4b288836ac3586933ca2
oemancia <oemancia@hotmail.com>
2022-05-31 13:56:33 -0600

    update libro 2

---
+++ app/controllers/BookTwoUnitTwo/data/dataUnitTwoPage48.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    question: "I ____ a student",
+    choices: ["AM", "IS", "ARE"],
+    answer: "AM",
+  },
+  {
+    question: "He ____ my father",
+    choices: ["AM", "IS", "ARE"],
+    answer: "IS",
+  },
+  {
+    question: "She ____ my sister",
+    choices: ["AM", "IS", "ARE"],
+    answer: "IS",
+  },
+  {
+    question: "She ____ my mother",
+    choices: ["AM", "IS", "ARE"],
+    answer: "IS",
+  },
+  {
+    question: "They ____ my parents",
+    choices: ["AM", "IS", "ARE"],
+    answer: "ARE",
+  },
+];
