e75cf1074ae0a612280411608fb8ad9dec867d44
AndreaTurcios <turciosbarquero@gmail.com>
2022-06-12 01:03:21 -0600

    cambios archivos

---
+++ views/dashboard/libro2unidad3.php
@@ -0,0 +1,501 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Agregamos Bootstrap -->
+    <link rel="stylesheet" href="../../resources/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    <title>Unit 1 - First Grade</title>
+    <link rel="shortcut icon" href="../../resources/img/logosinai.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../resources/css/bookstyles.css">
+</head>
+
+
+<body>
+    <!-- Previous Button -->
+    <button id="prev-btn">
+        <i class="fas fa-arrow-circle-left"></i>
+    </button>
+
+
+    <!-- Book -->
+    <div id="book" class="book">
+        <!-- Paper 1 -->
+        <div id="p1" class="paper">
+            <div class="front">
+                <div id="f1" class="front-content">
+                    <div>
+                        <img src="../../resources/img/BOOKS/SecondGrade/UnitThree/44.PNG" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b1" class="back-content">
+                    <div>
+                        <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/43.PNG" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 2 -->
+        <div id="p2" class="paper">
+            <div class="front">
+                <div id="f2" class="front-content">
+                    <div>
+                        <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/44.PNG" class="img-fluid float-start">
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b2" class="back-content">
+                    <div>
+                        <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/45.PNG" class="img-fluid float-start">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 3 -->
+        <div id="p3" class="paper">
+            <div class="front">
+                <div id="f3" class="front-content">
+                    <div>
+                        <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/46.PNG" class="img-fluid float-start">
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b3" class="back-content">
+                    <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/47.PNG" class="img-fluid float-start">
+                </div>
+            </div>
+        </div>
+        <!-- Paper 4 -->
+        <div id="p4" class="paper">
+            <div class="front">
+                <div id="f4" class="front-content">
+                <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/48.PNG" class="img-fluid float-start">
+                </div>
+            </div>
+            <div class="back">
+                <div id="b4" class="back-content">
+                    <div>
+                    <img src="../../resources/img/BOOKS/SecondGrade/UnitTwo/49.PNG" class="img-fluid float-start">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 5 -->
+        <div id="p5" class="paper">
+            <div class="front">
+                <div id="f5" class="front-content">
+                    <!-- inicio contenedor -->
+                    <h1>Page</h1>
+                    <!-- fin contenedor -->
+                </div>
+            </div>
+            <div class="back">
+                <div id="b5" class="back-content">
+                    <!-- inicio contenedor -->
+                    <h1>page</h1>
+                    <!-- fin contenedor -->
+                </div>
+            </div>
+        </div>
+        <!-- Paper 6 -->
+        <div id="p6" class="paper">
+            <div class="front">
+                <div id="f6" class="front-content">
+                    <div>
+                        <h1>Front 6</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b6" class="back-content">
+                    <div>
+                        <h1>Back 6</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 7 -->
+        <div id="p7" class="paper">
+            <div class="front">
+                <div id="f7" class="front-content">
+                    <div>
+                        <h1>Front 7</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b7" class="back-content">
+                    <div>
+                        <h1>Back 7</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 8 -->
+        <div id="p8" class="paper">
+            <div class="front">
+                <div id="f8" class="front-content">
+                    <div>
+                        <h1>Front 8</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b8" class="back-content">
+                    <div>
+                        <h1>Back 8</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 9 -->
+        <div id="p9" class="paper">
+            <div class="front">
+                <div id="f9" class="front-content">
+                    <div>
+                        <h1>Front 9</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b9" class="back-content">
+                    <div>
+                        <h1>Back 9</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 10 -->
+        <div id="p10" class="paper">
+            <div class="front">
+                <div id="f10" class="front-content">
+                    <div>
+                        <h1>Front 10</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b10" class="back-content">
+                    <div>
+                        <h1>Back 10</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 11 -->
+        <div id="p11" class="paper">
+            <div class="front">
+                <div id="f11" class="front-content">
+                    <div>
+                        <h1>Front 11</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b11" class="back-content">
+                    <div>
+                        <h1>Back 11</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 12 -->
+        <div id="p12" class="paper">
+            <div class="front">
+                <div id="f12" class="front-content">
+                    <div>
+                        <h1>Front 12</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b12" class="back-content">
+                    <div>
+                        <h1>Back 12</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 13 -->
+        <div id="p13" class="paper">
+            <div class="front">
+                <div id="f13" class="front-content">
+                    <div>
+                        <h1>Front 13</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b13" class="back-content">
+                    <div>
+                        <h1>Back 13</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 14 -->
+        <div id="p14" class="paper">
+            <div class="front">
+                <div id="f14" class="front-content">
+                    <div>
+                        <h1>Front 14</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b14" class="back-content">
+                    <div>
+                        <h1>Back 14</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 15 -->
+        <div id="p15" class="paper">
+            <div class="front">
+                <div id="f15" class="front-content">
+                    <div>
+                        <h1>Front 15</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b15" class="back-content">
+                    <div>
+                        <h1>Back 15</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 16 -->
+        <div id="p16" class="paper">
+            <div class="front">
+                <div id="f16" class="front-content">
+                    <div>
+                        <h1>Front 16</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b16" class="back-content">
+                    <div>
+                        <h1>Back 16</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 17 -->
+        <div id="p17" class="paper">
+            <div class="front">
+                <div id="f17" class="front-content">
+                    <div>
+                        <h1>Front 17</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b17" class="back-content">
+                    <div>
+                        <h1>Back 17</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 18 -->
+        <div id="p18" class="paper">
+            <div class="front">
+                <div id="f18" class="front-content">
+                    <div>
+                        <h1>Front 18</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b18" class="back-content">
+                    <div>
+                        <h1>Back 18</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 19 -->
+        <div id="p19" class="paper">
+            <div class="front">
+                <div id="f19" class="front-content">
+                    <div>
+                        <h1>Front 19</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b19" class="back-content">
+                    <div>
+                        <h1>Back 19</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 20 -->
+        <div id="p20" class="paper">
+            <div class="front">
+                <div id="f20" class="front-content">
+                    <div>
+                        <h1>Front 20</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b20" class="back-content">
+                    <h1>Back 20</h1>
+                </div>
+            </div>
+        </div>
+        <!-- Paper 21 -->
+        <div id="p21" class="paper">
+            <div class="front">
+                <div id="f21" class="front-content">
+                    <div>
+                        <h1>Front 21</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b21" class="back-content">
+                    <h1>Back 21</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Next Button -->
+    <button id="next-btn">
+        <i class="fas fa-arrow-circle-right"></i>
+    </button>
+</body>
+
+
+
+
+<!--inicio modal #1 -->
+<!-- <div id="ModalLibroJuego" class="modal fade">
+    <div class="container-fluid">
+        <form method="post" id="save-form">
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-title">Juego super genial que te enseña mucho</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="form-group">
+                                <label for="nombrejuego">Aquí irá el juego:</label>
+                                <input type="text" class="form-control" id="nombrejuego" name="nombrejuego" placeholder="El mejor juego del mundo xd" pattern="[a-zA-ZñÑáÁéÉíÍóÓúÚ\s]{1-50}" required minlength="3" maxlength="50" autocomplete="off" />
+                            </div>
+                            <br>
+                        </div>
+                        <br>
+                        
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar">Guardar</button><br>
+                        </div>
+                    </div>
+                </div>
+        </form>
+        </form>
+    </div>
+</div> -->
+<!-- fin modal #1 -->
+
+
+<!--inicio modal #1 -->
+<div id="ModalLibroUno" class="modal fade">
+    <div class="container-fluid">
+        <form method="post" id="game-one-form">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-title">Complete the words</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="form-group">
+                                <!-- columna -->
+                                <div class="container">
+                                    <div class="row row-cols-4">
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                        <!-- <div class="col"></div> -->
+                                        <div class="col border border-success">col</div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <br>
+                        </div>
+                        <br>
+                        <!-- Botones de Control -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar">Submit</button><br>
+                        </div>
+                    </div>
+                </div>
+        </form>
+        </form>
+    </div>
+</div>
+<!-- fin modal #1 -->
+
+
+
+
+</div>
+<script type="text/javascript" src="../../app/helpers/bookfix.js"></script>
+<script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
+<!-- Script de Bootstrap -->
+<script type="text/javascript" src="../../resources/js/autocomplete.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../../resources/js/vanilla-dataTables.min.js"></script>
+<script type="text/javascript" src="../../resources/js/sweetalert.min.js"></script>
+<script type="text/javascript" src="../../app/helpers/components.js"></script>
+<script type="text/javascript" src="../../app/controllers/account.js"></script>
+<script type="text/javascript" src="../../app/controllers/unidadtressegundogrado.js"></script>
+
+</html>
