77a22e942bc519604c368d098e46c4031ed77fe9
Geo-Stelar <diegoestrada2606@gmail.com>
2022-06-14 19:23:33 -0600

    Prueba 2

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -21,8 +21,8 @@ class Database
         $username = 'postgres';
         //Cambiar dependiendo de la persona, pero si pueden de un solo denle git ignore 
         //para no estar cambiando a cada rato
-        $passwordAndreaAndMazaandMajo = '123';
-
+        $passwordAndreaAndMazaandMajo = 'admin';
+        $passwordPrueba = 'admin1';
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:  host='.$server.';dbname='.$database.
         ';port=5432', $username, $passwordAndreaAndMazaandMajo);
