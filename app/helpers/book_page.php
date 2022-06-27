3ed7b8c3834b078b774049352719ad2f54d6f4a8
Erick Campos <erickcampos.rivas@gmail.com>
2022-06-26 23:02:33 -0600

    Libro 6 Pag 17

--- app/helpers/book_page.php
+++ app/helpers/book_page.php
@@ -45,7 +45,7 @@ class Book_Page
                 
                 </head>
                 <body style="background-image: url(../../resources/img/BOOKS/back.jpg);">
-                <input class="d-none" id="users" name="users" value="'. $_SESSION['id_empleado'] .'"/>                
+                <input class="d-none" id="users" name="users" value="' . $_SESSION['id_empleado'] . '"/>                
             ');
             } else if ($_SESSION['id_tipo_empleado'] == 2) { //Administrador
                 print(' <!DOCTYPE html>
@@ -182,9 +182,9 @@ class Book_Page
             
             </head>
             <body style="background-image: url(../../resources/img/BOOKS/back.jpg);">
-            <input type="hidden"  id="users" name="users" value="'. $_SESSION['id_cliente'] .'"/>
+            <input type="hidden"  id="users" name="users" value="' . $_SESSION['id_cliente'] . '"/>
             '
-            
+
             );
         } else {
             header('location: principal.php');
@@ -195,7 +195,7 @@ class Book_Page
     public static function footerTemplate($game)
     {
         // Se comprueba si existe una sesión de administrador para imprimir el pie respectivo del documento.
-       
+
         print('
         </body>
         <script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
@@ -212,10 +212,33 @@ class Book_Page
         </html>');
     }
 
+    public static function footerTemplate6($game)
+    {
+        // Se comprueba si existe una sesión de administrador para imprimir el pie respectivo del documento.
+
+        print('
+        </body>
+        <link rel="stylesheet" href="../../resources/js/wordfind/wordfind.css">
+        <script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
+        <!-- Script de Bootstrap -->
+        <script type="text/javascript" src="../../resources/js/autocomplete.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="../../resources/js/vanilla-dataTables.min.js"></script>   
+        <script type="text/javascript" src="../../resources/js/sweetalert.min.js"></script>
+        <script type="text/javascript" src="../../app/helpers/components.js"></script>
+        <script type="text/javascript" src="../../app/controllers/account.js"></script>
+        <script type="text/javascript" src="../../resources/js/wordfind/wordfind.js"></script>
+        <script type="text/javascript" src="../../resources/js/wordfind/wordfindgame.js"></script>
+        <script type="text/javascript" src="../../app/controllers/controladoreslibros/' . $game . '"></script>
+        <!-- <script type="text/javascript" src="../../app/controllers/"></script> -->
+        </html>');
+    }
+
     public static function footerTemplateBook2($game)
     {
         // Se comprueba si existe una sesión de administrador para imprimir el pie respectivo del documento.
-       
+
         print('
         </body>
         <script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
