cee663320a34c2ac1ff75e86e6ae3ff22a01f1ac
Maza17 <dinnx247@gmail.com>
2022-06-28 22:11:17 -0600

    Creacion y finalizacion de la A7U1L11

--- app/api/proceso_libro.php
+++ app/api/proceso_libro.php
@@ -709,6 +709,27 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'puntos incorrecto';
                 }
                 break;
+                case 'createactA7U1L11':
+                    $_POST = $libros->validateForm($_POST);
+                    if ($libros->setPromedio($_POST['pointsA7U1L11'])) {
+                        if ($libros->setLibro($_POST['idlibroA7U1L11'])) {
+                            if ($libros->setId($_POST['idclienteA7U1L11'])) {
+                                if ($libros->createRow()) {
+                                    $result['status'] = 1;
+                                    $result['message'] = 'success';
+                                } else {
+                                    $result['exception'] = Database::getException();;
+                                }
+                            } else {
+                                $result['exception'] = 'cliente incorrecto';
+                            }
+                        } else {
+                            $result['exception'] = 'libro incorrecto';
+                        }
+                    } else {
+                        $result['exception'] = 'puntos incorrecto';
+                    }
+                    break;
             default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
