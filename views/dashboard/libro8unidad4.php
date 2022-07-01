eb307c0fe43daff66fa7a5965750fa34664138fc
GeovannyChipagua <lgeovannycr@gmail.com>
2022-06-30 19:36:02 -0600

    Avance act5 Libro8Unidad4

--- views/dashboard/libro8unidad4.php
+++ views/dashboard/libro8unidad4.php
@@ -717,6 +717,104 @@ Book_Page::headerTemplate('Unidad 4');
 <!-- fin Actividad 4-->
 <!-- fin Region 14-->
 
+<!-- Actividad 5-->
+<!-- Region 15-->
+<div id="ModalLibroOcho15" class="modal fade" tabindex="-4">
+    <!-- <div class="container-fluid"> -->
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Select the correct sentences using quantifiers with groceries</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" id="unit4-act5">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+							<div class="col-md-8 align-items-center">
+								<!-- class="d-none" -->
+								<input type="text"  class="d-none" id="points5" name="points5">
+                                <input type="text"  class="d-none" id="idlibro5" name="idlibro5">
+								<input type="text"  class="d-none" id="idcliente5" name="idcliente5">
+							</div>
+						</div>
+
+                        <div class="row border box-select-5">
+							<div class="col-md-12">
+								<label for="act5-q1">1-</label>
+								<select name="act5-q1" id="act5-q1">
+									<option value="0" selected disabled>Choose</option>
+									<option value="1">I need to use the microwave</option>
+									<option value="2">We want a lot of bananas for the smoothie</option><!-- correcto -->
+									<option value="3">Where is the refrigerator?</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="row border box-select-5">
+							<div class="col-md-12">
+								<label for="act5-q2">2-</label>
+								<select name="act5-q2" id="act5-q2">
+									<option value="0" selected disabled>Choose</option>
+									<option value="1">I have few milk in the refrigerator</option><!-- correcto -->
+									<option value="2">We are very happy</option>
+									<option value="3">I don't have much time</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="row border box-select-5">
+							<div class="col-md-12">
+								<label for="act5-q3">3-</label>
+								<select name="act5-q3" id="act5-q3">
+									<option value="0" selected disabled>Choose</option>
+									<option value="1">We don't have much money</option>
+									<option value="2">There are several kinds of cakes on sale</option><!-- correcto -->
+									<option value="3">I'm very tired</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="row border box-select-5">
+							<div class="col-md-12">
+								<label for="act5-q4">4-</label>
+								<select name="act5-q4" id="act5-q4">
+									<option value="0" selected disabled>Choose</option>
+									<option value="1">I'm very hungry</option>
+									<option value="2">Can you give me some more of juice?</option><!-- correcto -->
+									<option value="3">It's too late</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="row border box-select-5">
+							<div class="col-md-12">
+								<label for="act5-q5">5-</label>
+								<select name="act5-q5" id="act5-q5">
+									<option value="0" selected disabled>Choose</option>
+									<option value="1">There are many cookies for your friends</option><!-- correcto -->
+									<option value="2">I'm busy all the time</option>
+									<option value="3">We need more space</option>
+								</select>
+							</div>
+						</div>
+
+                    </div>
+                    <br>
+                    <!-- Botones de Control -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar">Submit</button>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- fin Actividad 5-->
+<!-- fin Region 15-->
+
 <!-- --------------------------------- inicio plantilla footer  ---------------------------------	 -->
 <?php
 // Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
