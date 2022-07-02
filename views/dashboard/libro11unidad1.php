9df81fd4205126ebf4d5d3035fd0d734c2fd8e6e
Maza17 <dinnx247@gmail.com>
2022-07-01 22:55:54 -0600

    Creacion y finalizacion A9U1L11

--- views/dashboard/libro11unidad1.php
+++ views/dashboard/libro11unidad1.php
@@ -887,6 +887,80 @@ yepnope({
     </div>
 </div>
 
+<div id="ModalLibroNueve" class="modal fade" tabindex="-4">
+    <!-- <div class="container-fluid"> -->
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Complete the activity - Solve doubts about the content of the page</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" id="game-nine">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-8 align-items-center">
+                                <!-- class="d-none" -->
+                                <input type="text" class="d-none" id="pointsA9U1L11" name="pointsA9U1L11">
+                                <input type="text" class="d-none" id="idlibroA9U1L11" name="idlibroA9U1L11">
+                                <input type="text" class="d-none" id="idclienteA9U1L11" name="idclienteA9U1L11">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>What is something Frank always wanted to do?</h5>
+                                <select name="sentences" id="se1">
+                                    <option value="1">Be a good doctor</option>
+                                    <option value="2">Be a famous soccer player</option>
+                                    <option value="3">Be a actor</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>What would Katherine like to do in five years?</h5>
+                                <select name="sentences" id="se2">
+                                    <option value="1">Found a enterprise</option>
+                                    <option value="2">Build shes house</option>
+                                    <option value="3">Visit Egipt and Israel</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>How long have you been saving for that?</h5>
+                                <select name="sentences" id="se3">
+                                    <option value="1">During six years</option>
+                                    <option value="2">During four years</option>
+                                    <option value="3">During seven years</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>What was Melvin's childhood like?</h5>
+                                <select name="sentences" id="se4">
+                                    <option value="1">It was bored</option>
+                                    <option value="2">It was fun and full of love</option>
+                                    <option value="3">It was painful and sad</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <!-- Botones de Control -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn waves-effect blue tooltipped"
+                            data-tooltip="Guardar">Submit</button>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <!-- --------------------------------- inicio plantilla footer  ---------------------------------	 -->
 <?php
 // Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
